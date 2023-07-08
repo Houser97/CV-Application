a98b0621ed13f8a85fa35dd9cf0512692f04687b
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-07 20:16:18 -0600

    Create constants file and set NAV_ITEMS array

---
+++ src/assets/constants.tsx
@@ -0,0 +1,30 @@
+export const NAV_ITEMS = [
+    {
+        item: 'Home',
+        svg: 'home'
+    },
+    {
+        item: 'Profile',
+        svg: 'user'
+    },
+    {
+        item: 'Education',
+        svg: 'book'
+    },
+    {
+        item: 'Skills',
+        svg: 'receipt'
+    },
+    {
+        item: 'Certificates',
+        svg: 'award'
+    },
+    {
+        item: 'Experience',
+        svg: 'briefcase-alt'
+    },
+    {
+        item: 'References',
+        svg: 'link-external'
+    },
+]
