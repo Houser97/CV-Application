8e7788896606b1f0f8e2ecf820c9c075923b1d72
Arturo <arturo.riverar97@gmail.com>
2023-07-10 18:29:11 -0600

    Create Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,35 @@
+import { HOME_INFORMATION } from '../assets/constants'
+import '../styles/Home.css'
+
+const Home = () => {
+  return (
+    <div className='home' id='home'>
+        <div className="home__container section bd-grid">
+            <div className="home__data bd-grid">
+                <img src="" alt="profile" className="home__img" />
+
+                <h1 className="home__title">Arturo <b>Rivera</b></h1>
+                <h3 className="home__profession">Full-Stack Developer</h3>
+
+                <div>
+                    <a href="" className="home__button-movil"></a>
+                </div>
+            </div>
+
+            <div className="home__address bd-grid">
+                {
+                    HOME_INFORMATION.map(({data, svg}) => {
+                        return(
+                            <span className="home__information" key={`home-${svg}`}>
+                                <i className={`bx bx-${svg} home__icon`}></i> {data}
+                            </span>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
