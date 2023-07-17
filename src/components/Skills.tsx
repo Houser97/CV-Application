b6f75c57551286c90b77fde182ac199c1fab87b9
Arturo <arturo.riverar97@gmail.com>
2023-07-17 08:21:19 -0600

    Create Skills component and stylesheet

---
+++ src/components/Skills.tsx
@@ -0,0 +1,19 @@
+import '../styles/Skills.css'
+
+const Skills = () => {
+  return (
+    <div className='skills section' id='skills'>
+        <h2 className="section-title">Skills</h2>
+
+        <div className="skills__content bd-grid">
+            <ul className="skills__data">
+                <li className="skills__data">
+                    <span className="skills__circle"></span> React
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
