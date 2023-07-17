8faadff344d5cb3717ca0fb8fe81bc1d8b09ed07
Arturo <arturo.riverar97@gmail.com>
2023-07-17 08:29:29 -0600

    Set up Profile component with dummy data

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import '../styles/Profile.css'
+
+const Profile = () => {
+  return (
+    <div className='profile section' id='profile'>
+        <h2 className="section-title">Profile</h2>
+
+        <p className="profile__description">Here goes data</p>
+    </div>
+  )
+}
+
+export default Profile
