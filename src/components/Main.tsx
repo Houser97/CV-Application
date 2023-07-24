a625c8356aa74c985276bfb59da35b91698da507
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-23 18:55:48 -0600

    Create Main component to hold Resume component

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import '../styles/Main.css'
+import Resume from './Resume'
+
+const Main = () => {
+  return (
+    <div className='l-main bd__container'>
+        <Resume />
+    </div>
+  )
+}
+
+export default Main
