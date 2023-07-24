a26e489c915237c71ae67c44959b53f83ee7c042
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-23 18:54:46 -0600

    Create Experience componente with hardcoded data

---
+++ src/components/Experience.tsx
@@ -0,0 +1,39 @@
+import '../styles/Experience.css'
+
+const Experience = () => {
+  return (
+    <div className='experience section' id='experience'>
+        <h2 className="section-title">Experience</h2>
+
+        <div className="experience__container bf-grid">
+            <div className="experience__content">
+                <div className="experience__time">
+                    <span className="experience__rounder"></span>
+                    <span className="experience__line"></span>
+                </div>
+
+                <div className="experience__data bd-grid">
+                    <h3 className="experience__title">Engineer Projects Jr</h3>
+                    <span className="experience__company">From 2022 to current | Econ Tech</span>
+                    <p className="experience__description">Ignition</p>
+                </div>
+            </div>
+
+            <div className="experience__content">
+                <div className="experience__time">
+                    <span className="experience__rounder"></span>
+                    <span className="experience__line"></span>
+                </div>
+
+                <div className="experience__data bd-grid">
+                    <h3 className="experience__title">Engineer Projects Jr</h3>
+                    <span className="experience__company">From 2022 to current | Econ Tech</span>
+                    <p className="experience__description">Ignition</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
