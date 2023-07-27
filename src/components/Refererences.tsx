210ff1cb2c85fa0833c6d28b625d5b294597b65f
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-26 19:44:37 -0600

    Create References component and stylesheet

---
+++ src/components/Refererences.tsx
@@ -0,0 +1,31 @@
+import '../styles/References.css'
+
+const Refererences = () => {
+  return (
+    <div className='references section' id='references'>
+        <h2 className="section-title">References</h2>
+
+        <div className="references__container bd-grid">
+            <div className="references__content bd grid">
+                <span className="references__subtitle">Senior</span>
+                <h3 className="references__title">Housito</h3>
+                <ul className="references__contact">
+                    <li>Phone: 1204343</li>
+                    <li>Email: user@gmail.com</li>
+                </ul>
+            </div>
+
+            <div className="references__content bd grid">
+                <span className="references__subtitle">Senior</span>
+                <h3 className="references__title">Housito</h3>
+                <ul className="references__contact">
+                    <li>Phone: 1204343</li>
+                    <li>Email: user@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Refererences
